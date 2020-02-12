f711fbcb29e2756c782ba93ee1c25f18944a27a3
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-02-12 11:21:38 -0800

    Changed pk to slug

--- hobbies/urls.py
+++ hobbies/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from .views import HobbiesDetailView, HobbiesListView 
+from .views import HobbiesDetailView, HobbiesListView
 
 urlpatterns = [
     path('', HobbiesListView.as_view(), name='hobbies-home'),
-    path('hobbies-detail/<int:pk>', HobbiesDetailView.as_view(), name='hobbies-detail'),
+    path('hobbies-detail/<str:slug>', HobbiesDetailView.as_view(), name='hobbies-detail'),
 ]
