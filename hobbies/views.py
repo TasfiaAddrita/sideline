31b63e29a7bbabd9cd8c283e8d81d16d573dd3e0
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-02-12 11:52:19 -0800

    Provided a one-off default for datetimefield

--- hobbies/views.py
+++ hobbies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
 from .models import Hobbies
 
 class HobbiesListView(ListView):
@@ -19,7 +20,7 @@ class HobbiesDetailView(DetailView):
 
 class HobbiesEditView(UpdateView):
     """"""
-    model = Hobbie
+    model = Hobbies
     fields = '__all__'
     template_name = 'edit.html'
 
