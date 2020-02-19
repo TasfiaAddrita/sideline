1e4c1346cf1c9a9aecaf8851da3a15e007725128
ablades <audaris@gmail.com>
2020-02-19 12:11:17 -0800

    update user creation fields

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from .forms import SignUpForm
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     #Create view will automatically render the field named template_name
     template_name = 'signup.html'
