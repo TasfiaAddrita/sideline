5492d7b40d5c4c6ec23cc9b7425506b367d3fce5
ablades <audaris@gmail.com>
2020-02-19 12:37:28 -0800

    login form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from accounts.views import SignUpView
+from accounts.views import SignUpView, LoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
     # Accounts
-    path('signup', SignUpView.as_view(), name='sign-up-page'),
+    path('signup', SignUpView.as_view(), name='signup'),
+    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login')
+
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,15 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, View
 from .forms import SignUpForm
 
 
 class SignUpView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('login')
-    #Create view will automatically render the field named template_name
     template_name = 'signup.html'
+
+
+class LoginView(View):
+    authentication_form= AuthenticationForm
+    template_name= 'login.html'
